0521c9d6c259ed2af1d38703eb64739ef370b049
lieu <lieuzhenghong@gmail.com>
2023-01-12 17:53:25 +0800

    Update main.gd
    idk why

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,10 @@ const Character = preload("res://scenes/character.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    var c = Character.instantiate()
-    c.position = Vector2(50, 50)
-    self.add_child(c)
+	var c = Character.instantiate()
+	c.position = Vector2(50, 50)
+	self.add_child(c)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
