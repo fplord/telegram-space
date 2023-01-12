8621b8b0373991aea4cf794ae822ec81f9887dcd
Jesse Y. Cho <jesse@socean.fi>
2023-01-12 17:30:28 +0800

    wip: add simple network_handler using websocket

---
+++ singletons/network_handler.gd
@@ -0,0 +1,23 @@
+extends Node
+
+const SERVER_URL = "wss://<WEB-SOCKET-SERVER-URL>"
+
+var socket = WebSocketPeer.new()
+
+func _ready():
+    socket.connect_to_url(SERVER_URL)
+
+func _process(delta):
+    socket.poll()
+    var state = socket.get_ready_state()
+    if state == WebSocketPeer.STATE_OPEN:
+        while socket.get_available_packet_count():
+            print("Packet: ", socket.get_packet())
+    elif state == WebSocketPeer.STATE_CLOSING:
+        # Keep polling to achieve proper close.
+        pass
+    elif state == WebSocketPeer.STATE_CLOSED:
+        var code = socket.get_close_code()
+        var reason = socket.get_close_reason()
+        print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+        set_process(false) # Stop processing.
